7e70279f590f2ecb6afe6936ced228590da7e35e
vrushali-sonawane <svrushali7997@gmail.com>
2023-04-20 15:34:40 +0530

    first commit

---
+++ src/main/java/com/Bikkadit/SecondApplication/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.Bikkadit.SecondApplication.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+
+	public String getMsg11() {
+		String msg="Welcome to Bikkadit";
+		return msg;
+	}
+	
+	public String getmsg22() {
+		String msg="Good morning";
+	return msg;	
+	}
+}
---
+++ src/test/java/com/Bikkadit/SecondApplication/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bikkadit.SecondApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
