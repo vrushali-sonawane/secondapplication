2898111cd6ad0580108f4ece2973384b65dc4761
vrushali-sonawane <svrushali7997@gmail.com>
2023-04-20 15:41:30 +0530

    demo

---
+++ src/main/java/com/Bikkadit/SecondApplication/controller/SecondController.java
@@ -0,0 +1,15 @@
+package com.Bikkadit.SecondApplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.GetExchange;
+
+@Controller
+public class SecondController {
+
+	@GetMapping("/")
+	public String getMsg1() {
+		String  msg="Good Night";
+		return msg;
+	}
+}
